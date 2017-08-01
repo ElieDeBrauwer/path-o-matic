e322a796cc05c60f83835b302c320d5b9e821959
miyer <mahadevan.iyer@barco.com>
2017-08-01 10:20:18 -0700

    Updated postcloud.sh instructions

--- mw/postcloud.sh
+++ mw/postcloud.sh
@@ -1,2 +1,15 @@
 #!/bin/sh
-curl --user barco:b4rc0,BCD -k -i -X POST https://35.197.102.39/app/classify -H "Content-Type: text/xml" --data-binary "@./7.png"
+
+if [ $# -lt 4 ]
+then
+	echo "Usage: sh postcloud.sh <user> <password> <serverip> <pngfile>"
+	exit
+fi
+
+
+USER=$1
+PASSWORD=$2
+SERVERIP=$3
+PNGFILE=$4
+
+curl --user ${USER}:${PASSWORD} -k -i -X POST https://${SERVERIP}/app/classify -H "Content-Type: text/xml" --data-binary "@${PNGFILE}"
