78f15d98975106ec8df16dfc3201d0fdf24ac893
Elie De Brauwer <eliedebrauwer@gmail.com>
2017-08-01 20:43:26 +0000

    crop-o-matic.py: nonfunctional documentation update

--- input_processing/crop-o-matic.py
+++ input_processing/crop-o-matic.py
@@ -83,6 +83,8 @@ for annotation in annotation_list.getAnnotations():
                 print("Taking crop %d at %d %d" % (crop_cnt, x, y))
                 # A B
                 # C D
+
+                # Grab patches.
                 patch_a = np.array(mr_image.getUCharPatch(int(x), int(y), args.dim, args.dim, 0), dtype=np.uint8)
                 patch_b = np.array(mr_image.getUCharPatch(int(x + args.dim), int(y), args.dim, args.dim, 0), dtype=np.uint8)
                 patch_c = np.array(mr_image.getUCharPatch(int(x), int(y + args.dim), args.dim, args.dim, 0), dtype=np.uint8)
@@ -92,16 +94,17 @@ for annotation in annotation_list.getAnnotations():
                 patch_full = np.array(mr_image.getUCharPatch(int(x), int(y), 2 * args.dim, 2 * args.dim, 0), dtype=np.uint8)
 
 
+                # Grab masks.
                 mask_a = np.array(mr_mask.getUCharPatch(int(x), int(y), args.dim, args.dim, 0), dtype=np.uint8)
                 mask_b = np.array(mr_mask.getUCharPatch(int(x + args.dim), int(y), args.dim, args.dim, 0), dtype=np.uint8)
                 mask_c = np.array(mr_mask.getUCharPatch(int(x), int(y + args.dim), args.dim, args.dim, 0), dtype=np.uint8)
                 mask_d = np.array(mr_mask.getUCharPatch(int(x + args.dim), int(y + args.dim), args.dim, args.dim, 0), dtype=np.uint8)
-
                 mask_full = np.append(
                     np.append( mask_a, mask_b, axis=1),
                     np.append( mask_c, mask_d, axis=1), axis=0)
-
                 mask_full_bw = np.repeat(mask_full * 255, 3, axis=2) # Convert the mask in to a black-n-white array.
+
+                # Write images.
                 misc.imsave("patch_%05d_A_%d_%d_patch.png" % (crop_cnt, x, y),  patch_a)
                 misc.imsave("patch_%05d_B_%d_%d_patch.png" % (crop_cnt, x + args.dim, y),  patch_b)
                 misc.imsave("patch_%05d_C_%d_%d_patch.png" % (crop_cnt, x, y + args.dim),  patch_c)
@@ -110,6 +113,7 @@ for annotation in annotation_list.getAnnotations():
                 misc.imsave("patch_%05d_%d_%d_patch.png" % (crop_cnt, x, y),  patch_full)
                 misc.imsave("patch_%05d_%d_%d_mask.png" % (crop_cnt, x, y),  mask_full_bw)
 
+                # Calculate areas
                 metastases_area_a = np.count_nonzero(mask_a) / (args.dim * args.dim)
                 metastases_area_b = np.count_nonzero(mask_b) / (args.dim * args.dim)
                 metastases_area_c = np.count_nonzero(mask_c) / (args.dim * args.dim)
